cdee54a0cc77e7aca3c6b609c5ac5db9567b750b
OlumideOlufowose <olumideolufowose4@gmail.com>
2024-08-28 18:55:27 +0100

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,21 @@
 
+const installLogsPrinter = require('cypress-log-to-output').install;
+
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      installLogsPrinter(on, {
+        outputRoot: config.projectRoot + '/logs/',
+        outputTarget: {
+          'out.log': 'log',
+          'out.err': 'err',
+        },
+      });
+    },
+  },
+};
+
+
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
